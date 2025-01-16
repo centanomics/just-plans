19ed1ac5de566c72a151ffe95a944dec1ce9794c
centanomics <centanomics@gmail.com>
2025-01-15 22:22:01 -0500

    setup css pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Just Plans</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Just Plans</title>;
