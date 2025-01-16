920cfd77c4d469f788626192f82b8c491d124df4
centanomics <centanomics@gmail.com>
2025-01-15 23:46:55 -0500

    added html and css for the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,74 @@ import * as React from 'react';
 const IndexPage = () => {
   return (
     <main>
-      <h1>Just Plans</h1>
+      <h1>
+        Just <span className='teamColor'>Plans</span>
+      </h1>
+      <div className='court'>
+        <div className='spot spot-1'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-2'>
+          <div className='player'>
+            <span className='playerInitials'>CM</span>
+          </div>
+        </div>
+        <div className='spot spot-3'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-4'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-5'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-6'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-7'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-8'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-9'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+        <div className='spot spot-10'>
+          <div className='player'>
+            <span className='playerInitials'></span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <form>
+          <input
+            type='text'
+            id='initials'
+            minlength='2'
+            name='initials'
+            placeholder='Enter Player Initals'
+          />
+          <button>Add Player</button>
+        </form>
+        <button>Rotate</button>
+      </div>
     </main>
   );
 };
